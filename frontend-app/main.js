d4169dce75feedac1c9870952f40c85ccff3440b
timabloom <timabloom@proton.me>
2024-07-29 13:49:27 +0200

    feat: mobile screen size 320px style done

--- frontend-app/main.js
+++ frontend-app/main.js
@@ -1,5 +1,16 @@
 const quoteElement = document.querySelector(".card__quote");
 const buttonElement = document.querySelector(".card__button");
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const response = await fetch("http://localhost:5204/quote");
+    const quote = await response.json();
+    quoteElement.textContent = quote;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 buttonElement.addEventListener("click", async () => {
   try {
     const response = await fetch("http://localhost:5204/quote");
